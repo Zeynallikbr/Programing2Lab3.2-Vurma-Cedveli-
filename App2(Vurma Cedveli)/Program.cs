bda9ec1876afa2abd4d49530bad7c72abded82db
Ekber Zeynalli <zeynallikbr2@gmail.com>
2024-05-25 21:31:44 +0400

    Vurma cedveli

---
+++ App2(Vurma Cedveli)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+
+namespace App2_Vurma_Cedveli_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vurma Cedveli.");
+            Console.WriteLine(" 1  2  3  4  5  6  7  8  9  10");
+            Console.WriteLine("____________________________________");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write($"{i} | ");
+                for (int j = 1; j < 11; j++)
+                {
+                    //Console.WriteLine($"{i}");
+                    Console.Write($"{i *  j} ");
+                    if (i == 11)
+                    {
+                        Console.WriteLine("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Region1
+            //Console.WriteLine("Please, enter yours name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Pleace, enter yours surname: ");
+            //string surname = Console.ReadLine();
+            //Console.WriteLine("Name: " + name);
+            //Console.WriteLine("Surname: " + surname);
+            //Console.WriteLine($"Hello {surname} {name}");
+            #endregion
+            #region Region2
+            Console.WriteLine("Pleace, enter your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pleace, enter youur second Number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"1.Number {firstNumber}");
+            Console.WriteLine($"2.Number {secondNumber}");
+            Console.WriteLine("The sum of the first and second number")
+            #endregion
+            #region Region3
+
+            #endregion
+
+        }
+    }
+}
